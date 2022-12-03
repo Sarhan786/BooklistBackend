c8eda6e6a9c4e15b38b6cff717e2a2b21476c7eb
Sarhan786 <sarahan786@gmail.com>
2022-12-03 13:01:27 +0530

    Mongodb Connect and Post & Get req for Books completed

--- src/Model/books.js
+++ src/Model/books.js
@@ -2,6 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BooksSchema = new Schema({
+  //   Books: [
+  //     {
+  title: String,
+  author: String,
+  isbn: String,
+  publisher: String,
+  published_date: String,
+  description: String,
+  //     },
+  //   ],
+  //   user:{ type: Schema.Types.ObjectId, ref: "User" }
 });
 
-module.exports = mongoose.model("Books",BooksSchema);
+module.exports = mongoose.model("Books", BooksSchema);
